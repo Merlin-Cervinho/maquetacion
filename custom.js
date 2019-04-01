888038604880160e80b228be092abe7c1bb67c22
Merlin Cerviño Rodríguez <luismerlinrodriguez@gmail.com>
2019-04-01 21:26:15 +0100

    Yo me voy a cagar en la puta

---
+++ custom.js
@@ -0,0 +1,58 @@
+// Mostrar y ocultar el menú
+
+$(document).ready(function () {
+    'use strict';
+    $(window).scroll(function () {
+        'use strict';
+        if ($(window).scrollTop() < 80) {
+            $('.navbar').css({
+                'margin-top': '-100px',
+                'opacity': '0'
+            });
+
+            $('.navbar-default').css({
+                'background-color': 'rgba(59, 59, 59, 0)'
+            });
+
+        } else {
+            $('.navbar').css({
+                'margin-top': '0px',
+                'opacity': '1'
+            });
+
+            $('.navbar-default').css({
+                'background-color': 'rgba(255, 255, 255, 1)',
+                'border-color': '#fff'
+            });
+
+            $('.navbar-brand img').css({
+                'height': '35px',
+                'padding-top': '0px'
+            });
+
+            $('.navbar-nav > li > a').css({
+                'padding-top': '15px'
+            });
+        }
+    });
+});
+
+// Añadir desplazamiento sueave
+$(document).ready(function () {
+
+    'use strict';
+
+    $('.nav-item, #scroll-to-top').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+});
